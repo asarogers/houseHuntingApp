2a1b3dc30c6ee16fa381b147b78698a7ec312af7
Asa Rogers <cyberasasoftware@gmail.com>
2026-05-15 22:44:18 -0700

    Initial Commit

---
+++ Home Hunt App/Home_Hunt_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Home_Hunt_AppApp.swift
+//  Home Hunt App
+//
+//  Created by Atlas on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Home_Hunt_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
